a87df7b56dddfe9f567a2efa5d11e6e5c1cea793
Gustavo <gustovonogueira2019@gmail.com>
2023-05-01 11:13:09 -0300

    atualizando...

--- src/main/resources/META-INF/resources/js/js-login.js
+++ src/main/resources/META-INF/resources/js/js-login.js
@@ -1,10 +1,10 @@
 function validar(){
 
-    if (document.getElementById("modeloVeiculo").value == "") {
+    if (document.getElementById("nomeUsuario").value == "") {
         alert("O campo nome é obrigatório!");
         return false;
     }
-    if (document.getElementById("corVeiculo").value == "") {
+    if (document.getElementById("emailUsiario").value == "") {
         alert("O email é obrigatório!");
         return false;
     }
@@ -13,7 +13,7 @@ function validar(){
         alert("O campo Informações é obrigatório!");
         return false;
     }
-    if (document.getElementById("proprietarioVeiculo").value == "") {
+    if (document.getElementById("telefoneUsuario").value == "") {
         alert("O campo telefone é obrigatório!");
         return false;
     }
@@ -31,10 +31,10 @@ function inserirNaTabela(){
 
         //criando uma celula/coluna na linha criada e gardando o elemento criado em uma variável.
         let celula = linha.insertCell();
-        celula.innerText = document.getElementById("modeloVeiculo").value;
+        celula.innerText = document.getElementById("nomeUsuario").value;
 
         celula = linha.insertCell();
-        celula.innerText = document.getElementById("corVeiculo").value;
+        celula.innerText = document.getElementById("emailUsiario").value;
 
         celula = linha.insertCell();
         if (document.getElementById("opcaoNao").checked)
@@ -43,10 +43,21 @@ function inserirNaTabela(){
             celula.innerText = "Sim";
 
         celula = linha.insertCell();
-        celula.innerText = document.getElementById("proprietarioVeiculo").value;
+        celula.innerText = document.getElementById("telefoneUsuario").value;
         //
         // celula.append(criarBotaoRemover());
         //
         // limparFormulario();
+    }
+}
+
+function validarCampos() {
+    // Verifica se os campos anteriores foram preenchidos
+    if (document.getElementById('nomeUsuario').value !== "" && document.getElementById('emailUsiario').value !== "" && document.getElementById('telefoneUsuario').value !== "") {
+        // Se os campos foram preenchidos, redireciona o usuário para a próxima página
+        window.location.href = 'http://localhost:8080/';
+    } else {
+        // Se algum dos campos não foi preenchido, exibe uma mensagem de erro
+        alert('Por favor, preencha todos os campos para que possa acessar a página inicial do site');
     }
 }
