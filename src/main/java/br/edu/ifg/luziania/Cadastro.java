a2d355cf62e8776fcb96c7b4bae16cc8dac6a192
Gustavo <gustovonogueira2019@gmail.com>
2023-05-02 10:59:33 -0300

    atualizando...

--- src/main/java/br/edu/ifg/luziania/Cadastro.java
+++ src/main/java/br/edu/ifg/luziania/Cadastro.java
@@ -1,5 +1,25 @@
 package br.edu.ifg.luziania;
 
-public class Cadastro {
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+public class Cadastro {
+    private final Template cadastro;
+
+    public Cadastro(Template cadastro){
+        this.cadastro = cadastro;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/bs-cadastro")
+    public TemplateInstance login(){
+        return cadastro.instance();
+    }
 }
